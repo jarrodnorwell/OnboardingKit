721c6f336e9e49876b58edce7d40541f74c879b1
Jarrod Norwell <official.antique@gmail.com>
2025-09-28 07:56:09 +0700

    Allow subclassing

--- Sources/OnboardingKit/OnboardingController.swift
+++ Sources/OnboardingKit/OnboardingController.swift
@@ -64,7 +64,7 @@ open class OnboardingController : UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
